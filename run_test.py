dfb697ba31a22b3f8695ca7f3a42af9f9a5f4414
GaneshKumar <ganesh.kumar@datadoghq.com>
2020-11-16 18:31:52 -0500

    testing env vards

--- run_test.py
+++ run_test.py
@@ -1,1 +1,16 @@
+import os
+
 print("World Hello")
+print(os.environ['GITHUB_WORKFLOW'])
+print(os.environ['GITHUB_ACTOR'])
+print(os.environ['GITHUB_REPOSITORY'])
+print(os.environ['GITHUB_EVENT_NAME'])
+print(os.environ['GITHUB_EVENT_PATH'])
+print(os.environ['GITHUB_WORKSPACE'])
+print(os.environ['GITHUB_SHA'])
+print(os.environ['GITHUB_REF'])
+print(os.environ['GITHUB_HEAD_REF'])
+print(os.environ['GITHUB_BASE_REF'])
+print(os.environ['GITHUB_WORKSPACE'])
+print(os.environ['GITHUB_SHA'])
+
